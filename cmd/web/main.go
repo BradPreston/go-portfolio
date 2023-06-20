54b56aaca7da965ce75da833525636fbd9d976a7
bradpreston <bap5393@gmail.com>
2023-06-19 21:05:08 -0400

    Add application struct to main.go

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,9 +7,13 @@ type config struct {
     saticDir    string
 }
 
+type application struct {}
+
 func main() {
     var cfg config
     flag.StringVar(&cfg.addr, "addr", ":8080", "HTTP network address")
     flag.StringVar(&cfg.saticDir, "static-dir", "./ui/static", "Path to static assets")
     flag.Parse()
+
+    app := &application{}
 }
