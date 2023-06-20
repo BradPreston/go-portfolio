9dbf16a9b42d39d63e6cac830151d6722f94b1c9
bradpreston <bap5393@gmail.com>
2023-06-19 21:01:39 -0400

    Add config and flags to set config w/ defaults

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import "flag"
 
+type config struct {
+    addr        string
+    saticDir    string
+}
+
+func main() {
+    var cfg config
+    flag.StringVar(&cfg.addr, "addr", ":8080", "HTTP network address")
+    flag.StringVar(&cfg.saticDir, "static-dir", "./ui/static", "Path to static assets")
+    flag.Parse()
 }
