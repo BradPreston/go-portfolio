c964043f2a3d2bfa18b538c5a7e340a08d0a24f2
bradpreston <bap5393@gmail.com>
2023-06-25 19:25:02 -0400

    Add hobbies, playists, and contact links to templateData on home handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -26,18 +26,30 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    type project struct{
+    type link struct{
         Title   string
         Slug    string
     }
 
     preferredStack := []string{"NextJS","TypeScript","Node","Golang","SQL","NoSQL"}
     previousTech := []string{"Express","PHP","Laravel","Docker","MongoDB","PostgreSQL"}
-    projects := []project{
+    projects := []link{
         { "One With the Riverbed", "/one-with-the-riverbed" },
         { "Aaron's Music Service", "/aarons-music-service" },
         { "Get Pokemon Data From API", "/get-pokemon-data-from-api" },
         { "Kazoo48 Film Festival", "/kazoo48" },
+    }
+    hobbies := []string{"Reading on the couch","Playing video or board games","Playing guitar","Riding the stationary bike"}
+    playlists := []link{
+        {"Instrumental Madness","https://open.spotify.com/playlist/6yJYdOmWrsrV3F7rEEDxzT?si=2d738b5c33b24f16"},
+        {"Future Funk","https://open.spotify.com/playlist/37i9dQZF1DX3LDIBRoaCDQ?si=c39b289e8d4a43e4"},
+        {"Pokemon Lofi","https://open.spotify.com/playlist/59OrkYvGv0oM1KgPABU7nw?si=e09a56a6241e4cb1"},
+    }
+    contacts := []link{
+        {"Email", "mailto:bap5393@gmail.com"},
+        {"GitHub", "https://github.com/BradPreston"},
+        {"LinkedIn", "https://linkedin.com/in/brad-preston"},
+        {"Twitter", "https://twitter.com/braddoescoding"},
     }
 
     posts, err := internal.FetchPosts()
@@ -49,12 +61,18 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
         PreferredStack []string
         PreviousTech []string
         Posts []*internal.Post
-        Projects []project
+        Projects []link
+        Hobbies []string
+        Playlists []link
+        Contacts []link
     }{
         preferredStack,
         previousTech,
         posts,
         projects,
+        hobbies,
+        playlists,
+        contacts,
     }
 
 
